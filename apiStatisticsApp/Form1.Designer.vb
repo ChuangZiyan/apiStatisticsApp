2b90fcb92fb1251f0f0c4954bbb9740ac491035a
ChuangZiyan <ziyan.zhuang@h3platform.com>
2023-09-13 17:55:15 +0800

    加入專案檔案。

---
+++ apiStatisticsApp/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Data_URL_ListBox = New System.Windows.Forms.ListBox()
+        Me.Submit_Query_Button = New System.Windows.Forms.Button()
+        Me.Data_Query_Limit_NumericUpDown = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Job_Description_RichTextBox = New System.Windows.Forms.RichTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.Data_Query_Limit_NumericUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Data_URL_ListBox
+        '
+        Me.Data_URL_ListBox.FormattingEnabled = True
+        Me.Data_URL_ListBox.ItemHeight = 19
+        Me.Data_URL_ListBox.Location = New System.Drawing.Point(32, 83)
+        Me.Data_URL_ListBox.Name = "Data_URL_ListBox"
+        Me.Data_URL_ListBox.Size = New System.Drawing.Size(426, 441)
+        Me.Data_URL_ListBox.TabIndex = 0
+        '
+        'Submit_Query_Button
+        '
+        Me.Submit_Query_Button.Location = New System.Drawing.Point(364, 49)
+        Me.Submit_Query_Button.Name = "Submit_Query_Button"
+        Me.Submit_Query_Button.Size = New System.Drawing.Size(94, 29)
+        Me.Submit_Query_Button.TabIndex = 1
+        Me.Submit_Query_Button.Text = "送出"
+        Me.Submit_Query_Button.UseVisualStyleBackColor = True
+        '
+        'Data_Query_Limit_NumericUpDown
+        '
+        Me.Data_Query_Limit_NumericUpDown.Location = New System.Drawing.Point(118, 47)
+        Me.Data_Query_Limit_NumericUpDown.Name = "Data_Query_Limit_NumericUpDown"
+        Me.Data_Query_Limit_NumericUpDown.Size = New System.Drawing.Size(89, 27)
+        Me.Data_Query_Limit_NumericUpDown.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 19)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "最大筆數 : "
+        '
+        'Job_Description_RichTextBox
+        '
+        Me.Job_Description_RichTextBox.Location = New System.Drawing.Point(489, 154)
+        Me.Job_Description_RichTextBox.Name = "Job_Description_RichTextBox"
+        Me.Job_Description_RichTextBox.Size = New System.Drawing.Size(472, 370)
+        Me.Job_Description_RichTextBox.TabIndex = 4
+        Me.Job_Description_RichTextBox.Text = ""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(489, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 19)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "公司名稱 : "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(575, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(386, 27)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(489, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 19)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "聯繫方式 : "
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(575, 83)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(386, 27)
+        Me.TextBox2.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(489, 132)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(154, 19)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "工作敘述 (原始資料) : "
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(299, 48)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(53, 27)
+        Me.NumericUpDown1.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(213, 54)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(80, 19)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "間隔秒數 : "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1004, 600)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Job_Description_RichTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Data_Query_Limit_NumericUpDown)
+        Me.Controls.Add(Me.Submit_Query_Button)
+        Me.Controls.Add(Me.Data_URL_ListBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Data_Query_Limit_NumericUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Data_URL_ListBox As ListBox
+    Friend WithEvents Submit_Query_Button As Button
+    Friend WithEvents Data_Query_Limit_NumericUpDown As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Job_Description_RichTextBox As RichTextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label5 As Label
+End Class
---
+++ apiStatisticsApp/Form1.vb
@@ -0,0 +1,155 @@
+﻿Imports System.Net.Http
+Imports System.Threading.Tasks
+Imports System.Net.Http.Headers
+Imports System.Text
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+    Public currentDirectory As String = My.Application.Info.DirectoryPath
+
+    Private Async Sub Submit_Query_Button_Click(sender As Object, e As EventArgs) Handles Submit_Query_Button.Click
+
+
+        For offset As Integer = 0 To Data_Query_Limit_NumericUpDown.Value - 1 Step 20
+            Debug.WriteLine("Offset : " & offset & " Limit : " & offset + 20)
+
+        Next
+
+
+
+
+
+        Dim jsonString = Await Submit_API_Request_And_Generate_File(0, 5)
+        Dim jsonObject As JObject = JObject.Parse(jsonString)
+        Dim resultArray As JArray = jsonObject.SelectToken("data.job_search.result")
+
+        For Each item As JObject In resultArray
+            Debug.WriteLine(item.SelectToken("company.name"))
+            Data_URL_ListBox.Items.Add(item.SelectToken("company.name"))
+        Next
+
+
+
+        'Data_URL_ListBox.Items.Add("Test1")
+    End Sub
+
+
+
+    Public Async Function Submit_API_Request_And_Generate_File(offset As Integer, limit As Integer) As Task(Of String)
+
+        Dim requestData As New With {
+        .query = "
+            query QueryJobs (
+              $company: ID
+              $exclude: [ID]
+              $district: [ID]
+              $employment: [Employment]
+              $workingHour: [JobSearchWorkingHour]
+              $fromWorkingDaysPerWeek: Float
+              $fromWorkingHoursPerDay: Float
+              $hourlyRate: Float
+              $monthlyRate: Float
+              $jobType: [ID]
+              $limit: Int
+              $offset: Int
+              $term: String
+              $latlng: MLatLng
+            ) {
+              job_search(
+                company: $company
+                exclude: $exclude
+                term: $term,
+                district: $district,
+                employment: $employment,
+                from_working_days_per_week: $fromWorkingDaysPerWeek,
+                from_working_hours_per_day: $fromWorkingHoursPerDay,
+                hourly_rate: $hourlyRate,
+                monthly_rate: $monthlyRate,
+                job_type: $jobType,
+                limit: $limit,
+                offset: $offset,
+                working_hour: $workingHour,
+                latlng: $latlng
+              ) {
+                ...jobSearchResultFragment
+              }
+            }
+
+            fragment jobSearchResultFragment on JobSearchResult {
+              result {
+                ...jobFragmentCore
+              }
+              total
+            }
+
+            fragment jobFragmentCore on Job {
+              _id
+              address {
+                district_name
+                district_short_code
+              }
+              company {
+                company_logo_thumbnail
+                name
+              }
+              employment
+              from_hourly_rate
+              from_monthly_rate
+              job_name
+              slug {
+                locale
+                value
+              }
+            }
+            ",
+            .variables = New With {
+                .company = Nothing,
+                .district = New List(Of String)(),
+                .employment = New List(Of String)() From {"PartTime"},
+                .fromWorkingDaysPerWeek = Nothing,
+                .fromWorkingHoursPerDay = Nothing,
+                .hourlyRate = Nothing,
+                .jobType = New List(Of String)(),
+                .latlng = Nothing,
+                .limit = limit,
+                .monthlyRate = Nothing,
+                .offset = offset,
+                .term = "",
+                .workingHour = New List(Of String)()
+            }
+        }
+        ' Test Url
+        Dim apiUrl As String = "http://10.0.21.24:8089/v1/seek"
+
+
+        'Dim apiUrl As String = "https://api.moovup.com/v2/seeker"
+
+        Dim bearerToken As String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MGJkOTdiOC1iZjg1LTRhYzQtOTY4ZS01M2Q5NGMzYjJjMTgiLCJpYXQiOjE2OTQ1MDYwNTQsImlzcyI6Im1vb3Z1cCIsInN1YiI6IjdmN2JlMTZjLTVjMmUtNGEzMC05ZTQxLTA4ZDZkYjA1M2VjMCIsImFub255bW91cz8iOnRydWV9.2y2CpTlXOdvoGQ5ukfTag8IvIvyMMVTum21mZeZgV9A"
+
+        Using httpClient As New HttpClient()
+
+            httpClient.DefaultRequestHeaders.Authorization = New AuthenticationHeaderValue("Bearer", bearerToken)
+            Dim jsonRequestData As String = JsonConvert.SerializeObject(requestData)
+            Dim content As New StringContent(jsonRequestData, Encoding.UTF8, "application/json")
+
+            Dim response As HttpResponseMessage = Await httpClient.PostAsync(apiUrl, content)
+
+            If response.IsSuccessStatusCode Then
+                Dim responseBody As String = Await response.Content.ReadAsStringAsync()
+                Debug.WriteLine("############ responseBody: ############### ")
+                Debug.WriteLine(responseBody)
+                Job_Description_RichTextBox.Text = responseBody
+                Return responseBody
+            Else
+                Debug.WriteLine("http status code : " & response.StatusCode)
+                Job_Description_RichTextBox.Text = response.StatusCode.ToString()
+            End If
+        End Using
+
+
+
+        Return "error"
+
+    End Function
+End Class
